ddef2ce368a840e503e981443ec9d7809e235218
katenin <kateninouattara@gmail.com>
2025-10-17 13:55:19 +0000

    Delete simple_immobilier directory

--- simple_immobilier/suivie/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import DemandeSuivie
-
-@admin.register(DemandeSuivie)
-class DemandeSuivieAdmin(admin.ModelAdmin):
-    list_display = ('nom', 'email', 'numero_dossier', 'date_demande')
-    search_fields = ('nom', 'numero_dossier')
-    list_filter = ('date_demande',)
--- simple_immobilier/suivie/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class SuivieConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'suivie'
